2b1d3495ad0f2da8f93b5c348f16493766accca8
digitalatlantique <mick.info@yahoo.com>
2020-04-15 11:47:05 +0200

    [DEV] Partie 2 - Chapitre 8 - Maîtriser l'authentification - 2.2.1 MAJ état du service'

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {LayoutService} from '../../services/layout.service';
+import {User} from '../../../shared/models/user';
+import {Subscription} from 'rxjs';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'al-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
   public homePath = 'home';
   public loginPath = 'login';
   public registerPath = 'register';
 
-  constructor(private router: Router, private layoutService: LayoutService) { }
+  public user: User;
+
+  private subscription: Subscription;
+
+  constructor(private router: Router, private layoutService: LayoutService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.subscription = this.authService.user$.subscribe(user => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public isActive(page: string): boolean {
