7ff69fd162e0f4562e4a12c98eec4364a6be155d
digitalatlantique <mick.info@yahoo.com>
2020-04-17 14:36:14 +0200

    [DEV] Partie 2 - Chapitre 8 - Maîtriser l'authentification - 4.2 connexion et déconnexion automatique'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './core/services/auth.service';
+import {UsersService} from './core/services/users.service';
 
 @Component({
   selector: 'al-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  constructor(private authService: AuthService,
+              private usersService: UsersService) {
+  }
+
+  ngOnInit(): void {
+    this.tryAutoLogin();
+  }
+
+  private tryAutoLogin() {
+
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const expirationDate = + localStorage.getItem('expirationDate');
+      const now = new Date().getTime();
+
+      if (now < expirationDate) {
+        const userId = localStorage.getItem('userId');
+        this.usersService.get(userId, token).subscribe(user => {
+          this.authService.autoLogin(user);
+        });
+      }
+    }
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,7 +3,7 @@ import {BehaviorSubject, Observable, of} from 'rxjs';
 import {User} from '../../shared/models/user';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {catchError, finalize, switchMap, tap} from 'rxjs/operators';
+import {catchError, delay, finalize, switchMap, tap} from 'rxjs/operators';
 import {UsersService} from './users.service';
 import {ErrorService} from './error.service';
 import {LoaderService} from './loader.service';
@@ -13,6 +13,8 @@ import {Router} from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
+
+  private readonly EXPIRATION_TIME: number = 3600 * 1000;
 
   private user: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
 
@@ -45,9 +47,11 @@ export class AuthService {
       switchMap((pData: any) => {
         const userId: string = pData.localId;
         const jwt: string = pData.idToken;
+        this.saveAuthData(userId, jwt);
         return this.usersService.get(userId, jwt);
       }),
       tap(user => this.user.next(user)),
+      tap(_ => this.logoutTimer(this.EXPIRATION_TIME)),
       catchError(error => this.errorService.handleError(error)),
       finalize(() => this.loaderService.setLoading(false))
     );
@@ -79,16 +83,42 @@ export class AuthService {
           name: pName,
           email: pData.email
         });
+        this.saveAuthData(user.id, jwt);
         return this.usersService.save(user, jwt);
       }),
       tap(user => this.user.next(user)),
+      tap(_ => this.logoutTimer(this.EXPIRATION_TIME)),
       catchError(error => this.errorService.handleError(error)),
       finalize(() => this.loaderService.setLoading(false))
     );
   }
 
   public logout(): void {
+
+    localStorage.removeItem('userId');
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+
     this.user.next(null);
     this.router.navigate(['/login']);
+  }
+
+  public autoLogin(user: User) {
+    this.user.next(user);
+    this.router.navigate(['app/dashboard']);
+  }
+
+  private logoutTimer(expirationTime: number): void {
+    of(true).pipe(
+      delay(expirationTime)
+    ).subscribe(_ => this.logout());
+  }
+
+  private saveAuthData(userId: string, token: string) {
+    const now = new Date();
+    const expirationDate = (now.getTime() + this.EXPIRATION_TIME).toString();
+    localStorage.setItem('expirationDate', expirationDate);
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
   }
 }
